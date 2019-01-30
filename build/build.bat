db6e570287e4c2a4f0b963725ee1439e77d14826
Esperanza <elopezaguilera@kx.com>
2019-01-30 11:10:49 +0000

    chage call to appveyor

--- build/build.bat
+++ build/build.bat
@@ -18,7 +18,6 @@ if not defined QLIC_KC (
 set PATH=%OP%
 call build\getkdb.bat                                || goto :error
 set PATH=C:\Miniconda3-x64;C:\Miniconda3-x64\Scripts;%PATH%
-call "C:\Miniconda3-x64\Scripts\activate.bat"
 mkdir embedpy
 cd embedpy
 echo getembedpy"latest" | q ..\build\getembedpy.q -q || goto :error
